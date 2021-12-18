afc10e21e5628eddeb234b3fb2795d9b26dabc4c
viryal23 <nadaviryal23@gmail.com>
2021-12-18 23:56:52 +0700

    JOIN, UNION, SUB-QUERY, FUNCTION

--- SQL/Latihan SQL.sql
+++ SQL/Latihan SQL.sql
@@ -1,5 +1,5 @@
 --CREATE DATABASE
-CREATE DATABASE transaction;
+CREATE DATABASE transactions;
 
 --CREATE TABLE product_types
 CREATE TABLE product_types (
@@ -52,6 +52,7 @@ CREATE TABLE payment_methods (
 CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
     status SMALLINT,
+    name VARCHAR(255),
     dob DATE,
     gender CHAR(1),
     created_at TIMESTAMP,
@@ -104,22 +105,22 @@ VALUES
 --Insert 2 product dengan product type id = 1, dan operators id = 3
 INSERT INTO products(product_type_id, operator_id, code, name, status, created_at, updated_at)
 VALUES
-    (1, 3, 'SI1', 'How to Win Friends & Influence People', 11, current_timestamp, NULL),
-    (1, 3, 'SI2', 'Atomic Habits', 12, current_timestamp, NULL);
+    (1, 3, 'SI1', 'How to Win Friends & Influence People', 1, current_timestamp, NULL),
+    (1, 3, 'SI2', 'Atomic Habits', 1, current_timestamp, NULL);
 
 --Insert 3 product dengan product type id = 2, dan operators id = 1
 INSERT INTO products(product_type_id, operator_id, code, name, status, created_at, updated_at)
 VALUES
-    (2, 1, 'P1', 'The Psychology of Money', 21, current_timestamp, NULL),
-    (2, 1, 'P2', 'Peak Mind', 22, current_timestamp, NULL),
-    (2, 1, 'P3', 'Getting to Zero', 23, current_timestamp, NULL);
+    (2, 1, 'P1', 'The Psychology of Money', 1, current_timestamp, NULL),
+    (2, 1, 'P2', 'Peak Mind', 1, current_timestamp, NULL),
+    (2, 1, 'P3', 'Getting to Zero', 1, current_timestamp, NULL);
 
 --Insert 3 product dengan product type id = 3, dan operators id = 4
 INSERT INTO products(product_type_id, operator_id, code, name, status, created_at, updated_at)
 VALUES
-    (3, 4, 'R1', 'Creating Destiny', 31, current_timestamp, NULL),
-    (3, 4, 'R2', 'Perfect Romance', 32, current_timestamp, NULL),
-    (3, 4, 'R3', 'You Told Me So', 33, current_timestamp, NULL);
+    (3, 4, 'R1', 'Creating Destiny', 1, current_timestamp, NULL),
+    (3, 4, 'R2', 'Perfect Romance', 1, current_timestamp, NULL),
+    (3, 4, 'R3', 'You Told Me So', 1, current_timestamp, NULL);
 
 --Insert product description pada setiap product
 INSERT INTO product_descriptions(description, created_at, updated_at)
@@ -141,13 +142,13 @@ VALUES
     ('Debit Card', 1, current_timestamp,NULL);
 
 --Insert 5 user pada tabel user
-INSERT INTO users(status, dob, gender, created_at, updated_at)
+INSERT INTO users(status, name, dob, gender, created_at, updated_at)
 VALUES
-    (1, '2001-01-23', 'P', current_timestamp, NULL),
-    (1, '2000-11-13', 'L', current_timestamp, NULL),
-    (1, '2001-12-08', 'P', current_timestamp, NULL),
-    (1, '2001-04-02', 'L', current_timestamp, NULL),
-    (1, '2001-05-20', 'P', current_timestamp, NULL);
+    (1, 'Virana', '2001-01-23', 'P', current_timestamp, NULL),
+    (1, 'Dhani', '2000-11-13', 'L', current_timestamp, NULL),
+    (1, 'Hasna', '2001-12-08', 'P', current_timestamp, NULL),
+    (1, 'Raditya', '2001-04-02', 'L', current_timestamp, NULL),
+    (1, 'Cahya', '2001-05-20', 'P', current_timestamp, NULL);
 
 --Insert 3 transaksi di masing-masing user
 INSERT INTO transactions(user_id, payment_method_id, status, total_qty, total_price, created_at, updated_at)
@@ -219,23 +220,18 @@ VALUES
 
 --SELECT
 --Tampilkan nama user/pelanggan dengan gender laki-laki/M
---Users tidak memiliki atribut nama
-SELECT * FROM users WHERE gender = 'L';
---Jika users memiliki atribut nama
-SELECT name users WHERE gender = 'L';
+SELECT name FROM users WHERE gender = 'L';
 
 --Tampilkan product dengan id = 3
 SELECT * FROM products WHERE id = 3;
 
 --Tampilkan data pelanggan yang created_at dalam range 7 hari ke belakang dan mempunyai nama mengandung kata 'a'
---Anggap users memiliki atribut nama
 SELECT * FROM users WHERE created_at > current_date - 7 and name like '%a%';
 
 --Hitung jumlah user/pelanggan dengan status gender perempuan
 SELECT COUNT(*) FROM users WHERE gender = 'P';
 
 --Tampilkan data pelanggan dengan urutan sesuai nama abjad
---Anggap users memiliki atribut nama
 SELECT * FROM users ORDER BY name;
 
 --Tampilkan 5 data pada data product
@@ -254,3 +250,56 @@ DELETE FROM products WHERE id = 1;
 
 --Delete data pada tabel product dengan product type id 1
 DELETE FROM products WHERE product_type_id = 1;
+
+--JOIN, UNION, SUB-QUERY, FUNCTION
+--Gabungkan data transaksi dari user id 1 dan user id 2;
+SELECT * FROM users u INNER JOIN transactions t ON u.id = t.user_id WHERE u.id = 1 or u.id = 2;
+
+--Tampilkan jumlah harga transaksi user id 1
+SELECT SUM(total_price) AS total_price FROM transactions WHERE user_id = 1;
+
+--Tampilkan total transaksi dengan product type 2
+SELECT COUNT(transaction_id) AS total_transactions FROM transaction_details td INNER JOIN products p ON p.id = td.product_id WHERE p.product_type_id = 2;
+
+--Tampilkan semua field table product dan field name table product type yang saling berhubungan
+SELECT p.*, pt.name FROM products p INNER JOIN product_types pt ON p.product_type_id = pt.id;
+
+--Tampilkan semua field table transaction, field name table product dan field name table user
+SELECT t.*, p.name AS product_name, u.name AS user_name FROM users u INNER JOIN transactions t ON u.id = t.user_id
+INNER JOIN transaction_details td ON t.id = td.transaction_id
+INNER JOIN products p ON p.id = td.product_id;
+
+--Buat function setelah data transaksi dihapus maka transaction detail terhapus juga dengan transaction id yang dimaksud
+CREATE FUNCTION delete_transaction()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    DELETE FROM transaction_details WHERE transaction_id = old.id;
+    RETURN old;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_transaction
+BEFORE DELETE ON transactions
+FOR EACH ROW
+EXECUTE PROCEDURE delete_transaction();
+
+--Buat function setelah data transaksi detail dihapus maka data total_qty terupdate berdasarkan qty data transaction id yang dihapus
+CREATE FUNCTION update_total_qty()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+    UPDATE transactions SET total_qty = total_qty - old.qty WHERE id = old.transaction_id;
+    RETURN old;
+END;
+$$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_total_qty
+BEFORE DELETE ON transaction_details 
+FOR EACH ROW 
+EXECUTE PROCEDURE update_total_qty();
+
+--Tampilkan data products yang tidak pernah ada di table transaction_details dengan sub-query
+SELECT * FROM products WHERE id NOT IN (SELECT product_id FROM transaction_details GROUP BY product_id);
